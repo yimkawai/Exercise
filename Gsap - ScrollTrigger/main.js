b6b9b39a4f720f506b745b2f4ec3719eecc1d7f4
kw <yimkawaii@gmail.com>
2022-04-19 23:11:11 -0400

    gsap.to/from/fromTo

--- Gsap - ScrollTrigger/main.js
+++ Gsap - ScrollTrigger/main.js
@@ -1,7 +1,11 @@
-gsap.to(".a", {
+gsap.fromTo(".a", {
+    x: 700,
+    y: 400,
+    opacity: 0
+}, {
     x: 400,
     y: 200,
-    scale: 2,
-    rotation: 360,
+    scale: 3,
+    opacity: 1,
     duration: 3
 });
